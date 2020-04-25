baeaa6cc13a42cf36aedd0444b18b1f2297984be
matthewg372 <59273107+matthewg372@users.noreply.github.com>
2020-04-25 12:40:42 -0500

    created blue print and makes a car in postman

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 import models
+from resources.cars import cars
 
 DEBUG=True
 PORT=8000
 
 app = Flask(__name__)
+app.register_blueprint(cars, url_prefix='/api/v1/cars')
 
 @app.route('/')
 def hello():
--- resources/cars.py
+++ resources/cars.py
@@ -1,1 +1,22 @@
+import models
+from flask import Blueprint , request, jsonify
+from playhouse.shortcuts import model_to_dict 
+
+cars = Blueprint('cars', 'cars')
+
+@cars.route('/', methods=['POST'])
+def create_car():
+	payload = request.get_json()
+	new_car = models.Car.create(
+		model=payload['model'], 
+		make=payload['make'],
+		year=payload['year'],
+		suv=payload['suv']
+		)
+	car_dict = model_to_dict(new_car)
+	return jsonify(
+      data=car_dict, 
+      message='Successfully created dog!',
+      status=201
+    ), 201
 
