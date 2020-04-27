4d586107b4d0afa7fba9650701e06dbdc2bfb7f1
matthewg372 <59273107+matthewg372@users.noreply.github.com>
2020-04-27 17:43:25 -0500

    connected cors

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 import models
 from resources.cars import cars
+from flask_cors import CORS
 
 DEBUG=True
 PORT=8000
 
 app = Flask(__name__)
-# cors = CORS(app, origins=['http://localhost:3000'],supports_credentials=True)
+cors = CORS(app, origins=['http://localhost:3000'],supports_credentials=True)
 app.register_blueprint(cars, url_prefix='/api/v1/cars')
 
 @app.route('/')
