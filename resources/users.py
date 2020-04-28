5d49fb0067949dc084f14ccbd1bbce13516296cd
matthewg372 <59273107+matthewg372@users.noreply.github.com>
2020-04-28 17:34:31 -0500

    register is connected for user

--- resources/users.py
+++ resources/users.py
@@ -1,5 +1,8 @@
 import models
 from flask import Blueprint, request, jsonify
+from flask_bcrypt import generate_password_hash, check_password_hash
+from playhouse.shortcuts import model_to_dict
+
 
 users = Blueprint('users', 'users')
 
@@ -7,3 +10,34 @@ users = Blueprint('users', 'users')
 @users.route('/', methods=['GET'])
 def user_resource():
 	return "connected"
+
+@users.route('/register', methods=['POST'])
+def register():
+	payload = request.get_json()
+	payload['email'] = payload['email'].lower()
+	payload['username'] = payload['username'].lower()
+	try:
+		models.User.get(models.User.email == payload['email'])
+		return jsonify(
+			data={},
+			message=f"a user with {payload['email']} already exists",
+			status=401
+			), 401
+
+	except models.DoesNotExist:
+		pw_hash = generate_password_hash(payload['password'])
+
+		created_user = models.User.create(
+			username=payload['username'],
+			email=payload['email'],
+			password=pw_hash
+	    )
+		created_user_dict = model_to_dict(created_user)
+		created_user_dict.pop('password')
+
+
+		return jsonify(
+			data=created_user_dict,
+			message=f"Sucessfully registered user {created_user_dict['email']}",
+			status=201
+		), 201
