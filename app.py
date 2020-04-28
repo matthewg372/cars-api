396d79c660598a47e8fc984b875572272a9bacf4
matthewg372 <59273107+matthewg372@users.noreply.github.com>
2020-04-28 17:21:26 -0500

    tables being made for cars and users

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ from flask import Flask
 import models
 from resources.cars import cars
 from flask_cors import CORS
+# from resources.users import users
 
 DEBUG=True
 PORT=8000
 
 app = Flask(__name__)
-cors = CORS(app, origins=['http://localhost:3000'],supports_credentials=True)
+cors = CORS(cars, origins=['http://localhost:3000'],supports_credentials=True)
+# cors = CORS(user, origins=['http://localhost:3000'],supports_credentials=True)
 app.register_blueprint(cars, url_prefix='/api/v1/cars')
+# app.register_blueprint(users, url_prefix='/api/v1/users')
 
 @app.route('/')
 def hello():
--- models.py
+++ models.py
@@ -1,9 +1,18 @@
 from peewee import *
+from flask_login import UserMixin
 
 DATABASE = SqliteDatabase('cars.sqlite')
 
 
+class User(UserMixin, Model):
+	username=CharField(unique=True)
+	email=CharField(unique=True)
+	password=CharField()
+	class Meta:
+		database = DATABASE
+
 class Car(Model):
+	user = ForeignKeyField(User, backref='cars')
 	model = CharField()
 	make = CharField()
 	year = BitField()
@@ -13,6 +22,6 @@ class Car(Model):
 
 def initialize():
 	DATABASE.connect()
-	DATABASE.create_tables([Car], safe=True)
+	DATABASE.create_tables([User, Car], safe=True)
 	print('connected and create_tables')
 	DATABASE.close()
